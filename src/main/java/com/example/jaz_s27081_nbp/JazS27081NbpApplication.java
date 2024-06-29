ba19ebe98fb7c8fc2eadffbb5a312bb5b6f43f2d
Ziem0 <ziemekorlikowski@gmail.com>
2024-06-29 15:04:33 +0200

    first commit

---
+++ src/main/java/com/example/jaz_s27081_nbp/JazS27081NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jaz_s27081_nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS27081NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS27081NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jaz_s27081_nbp/JazS27081NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaz_s27081_nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS27081NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
